0e708a555da85a4f06069b46bd70cf9cd521e571
Triet NGUYEN <tr_nguye@insa-toulouse.fr>
2025-11-20 13:14:30 +0100

    feat: Add robot_state_publisher to both launch file (geicar and dev). Joint_state_publisher is also added to dev launch file for testing purpose

--- raspberryPI3/ros2_ws/src/geicar_start/launch/dev.launch.py
+++ raspberryPI3/ros2_ws/src/geicar_start/launch/dev.launch.py
@@ -1,10 +1,48 @@
 from launch import LaunchDescription
+from launch.substitutions import Command
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    urdf_model_path = os.path.join(
+        get_package_share_directory('geicar_description'),
+        'src',
+        'description',
+        'geicar_description.urdf'
+    )
     ld = LaunchDescription()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': Command(['xacro ', str(urdf_model_path)])}],
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', str(urdf_model_path)])}],
+    )
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory('geicar_start'), 'rviz', 'config.rviz')]
+    )
+
+
 
     joystick_node = Node(
         package="joystick",
@@ -41,11 +79,14 @@ def generate_launch_description():
         emulate_tty=True
     )
 
-
     ld.add_action(joystick_node)
     ld.add_action(joystick_to_cmd_node)
     ld.add_action(car_control_node)
     ld.add_action(imu_filter_madgwick_node)
     ld.add_action(system_check_node)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(joint_state_publisher_gui_node)
+    ld.add_action(rviz_node)
 
     return ld
