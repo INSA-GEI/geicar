dc754943fe700b580b1cffe4748d5a97167406d5
Triet NGUYEN <tr_nguye@insa-toulouse.fr>
2025-11-24 15:32:12 +0100

    fix: fix errors in lidar rotation link & jetson launch file

--- jetsonNano/ros2_ws/src/geicar_start_jetson/launch/geicar.jetson.launch.py
+++ jetsonNano/ros2_ws/src/geicar_start_jetson/launch/geicar.jetson.launch.py
@@ -50,10 +50,10 @@ def generate_launch_description():
             name='rplidar_node',
             parameters=[{'channel_type':'serial',
                          'serial_port': '/dev/ttyUSB0',
-                         'serial_baudrate': '256000',
+                         'serial_baudrate': 256000,
                          'frame_id': 'rplidar_link',
-                         'inverted': 'false',
-                         'angle_compensate': 'true',
+                         'inverted': False,
+                         'angle_compensate': True,
                          'scan_mode': 'Sensitivity'}],
             output='screen',
             condition=UnlessCondition(LaunchConfiguration('disable_lidar'))
