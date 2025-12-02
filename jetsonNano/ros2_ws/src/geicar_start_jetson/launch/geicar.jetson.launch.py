e9748b4f83f3e873c43c2fddaf9953b178e67c7e
Triet NGUYEN <tr_nguye@insa-toulouse.fr>
2025-12-02 21:49:39 +0100

    feat: add lidar odometry and robot_localization node for fusing Lidar odom with IMU

--- jetsonNano/ros2_ws/src/geicar_start_jetson/launch/geicar.jetson.launch.py
+++ jetsonNano/ros2_ws/src/geicar_start_jetson/launch/geicar.jetson.launch.py
@@ -9,6 +9,8 @@ from launch.conditions import UnlessCondition # Use IfCondition for "enable" fla
 def generate_launch_description():
     ld = LaunchDescription()
 
+    pkg_share = get_package_share_directory('geicar_start_jetson')
+
     # --- Declare Launch Arguments ---
     # We create a "disable" argument for each node. 
     # By default, they are 'false' (meaning the node IS launched).
@@ -18,6 +20,12 @@ def generate_launch_description():
         'disable_lidar',
         default_value='false',
         description='Disable the RPLidar node'
+    )
+
+    declare_disable_lio_arg = DeclareLaunchArgument(
+        'disable_lio',
+        default_value='false',
+        description='Disable the Laser Odometry node'
     )
 
     declare_disable_camera_arg = DeclareLaunchArgument(
@@ -38,6 +46,12 @@ def generate_launch_description():
         description='Disable the bridge node'
     )
 
+    declare_use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation time (Gazebo/Bag) if true'
+    )
+
     # --- Define Nodes with Conditions ---
 
     # We add a 'condition' to each node.
@@ -54,7 +68,8 @@ def generate_launch_description():
                          'frame_id': 'rplidar_link',
                          'inverted': False,
                          'angle_compensate': True,
-                         'scan_mode': 'Sensitivity'}],
+                         'scan_mode': 'Sensitivity'},
+                         {'use_sim_time': LaunchConfiguration('use_sim_time')}],
             output='screen',
             condition=UnlessCondition(LaunchConfiguration('disable_lidar'))
     )
@@ -65,7 +80,8 @@ def generate_launch_description():
         package="usb_cam",
         executable="usb_cam_node_exe",
         namespace="usb_cam_0",
-        parameters=[os.path.join(usb_cam_share, 'config', 'params_1.yaml')],
+        parameters=[{os.path.join(usb_cam_share, 'config', 'params_1.yaml')},
+                    {'use_sim_time': LaunchConfiguration('use_sim_time')}],
         emulate_tty=True,
         condition=UnlessCondition(LaunchConfiguration('disable_camera'))
     )
@@ -74,7 +90,8 @@ def generate_launch_description():
         package="usb_cam",
         executable="usb_cam_node_exe",
         namespace="usb_cam_1",
-        parameters=[os.path.join(usb_cam_share, 'config', 'params_2.yaml')],
+        parameters=[{os.path.join(usb_cam_share, 'config', 'params_2.yaml')},
+                    {'use_sim_time': LaunchConfiguration('use_sim_time')}],
         emulate_tty=True,
         condition=UnlessCondition(LaunchConfiguration('disable_camera'))
     )
@@ -84,6 +101,7 @@ def generate_launch_description():
         package="system_check_ack",
         executable="system_check_ack_node",
         emulate_tty=True,
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
         condition=UnlessCondition(LaunchConfiguration('disable_system_check'))
     )
 
@@ -91,16 +109,48 @@ def generate_launch_description():
         package="network_hmi",
         executable="bridge_node",
         emulate_tty=True,
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
         condition=UnlessCondition(LaunchConfiguration('disable_bridge'))
+    )
+
+    rf2o_laser_odometry_node = Node(
+        package='rf2o_laser_odometry',
+        executable='rf2o_laser_odometry_node',
+        name='rf2o_laser_odometry',
+        output='screen',
+        parameters=[{
+            'laser_scan_topic' : '/scan',
+            'odom_topic' : '/odom/laser_odom',
+            'publish_tf' : False,
+            'base_frame_id' : 'base_link',
+            'odom_frame_id' : 'odom',
+            'init_pose_from_topic' : '',
+            'freq' : 10.0},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}],
+        condition=UnlessCondition(LaunchConfiguration('disable_lio'))
+    )
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        remappings=[
+            ('/odometry/filtered', '/odom'),     # Rename output odom
+        ],
+        parameters=[{os.path.join(pkg_share, 'config/ekf.yaml')},
+                    {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
     # --- Add Actions to Launch Description ---
 
     # Add the argument declarations
     ld.add_action(declare_disable_lidar_arg)
+    ld.add_action(declare_disable_lio_arg)
     ld.add_action(declare_disable_camera_arg)
     ld.add_action(declare_disable_system_check_arg)
     ld.add_action(declare_disable_bridge_arg)
+    ld.add_action(declare_use_sim_time_arg)
 
     # Add the nodes (they will only be executed if their condition is met)
     ld.add_action(lidar_node)
@@ -108,5 +158,7 @@ def generate_launch_description():
     ld.add_action(camera_node_2)
     ld.add_action(system_check_ack_node)
     ld.add_action(bridge_node)
+    ld.add_action(rf2o_laser_odometry_node)
+    ld.add_action(robot_localization_node)
 
     return ld
