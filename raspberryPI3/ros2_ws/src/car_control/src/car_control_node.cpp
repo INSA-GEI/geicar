471f0808d4488884d16a2182afa48a32906fd339
hamrouni <iyedhamrouni3@gmail.com>
2025-12-04 10:21:31 +0100

    feat: Add node that converts cmd_vel(Twist) to a car command

--- raspberryPI3/ros2_ws/src/car_control/src/car_control_node.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/car_control_node.cpp
@@ -11,6 +11,8 @@
 #include "interfaces/msg/steering_calibration.hpp"
 #include "interfaces/msg/joystick_order.hpp"
 #include "interfaces/msg/ultrasonic.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 #include "std_srvs/srv/empty.hpp"
 
@@ -27,7 +29,7 @@ class car_control : public rclcpp::Node {
 
 public:
     car_control()
-    : Node("car_control_node")
+    : Node("car_control_node_iyed")
     {
         start = false;
         mode = 0;
@@ -43,7 +45,7 @@ public:
 
         publisher_can_= this->create_publisher<interfaces::msg::MotorsOrder>("motors_order", 10);
 
-        publisher_steeringCalibration_ = this->create_publisher<interfaces::msg::SteeringCalibration>("steering_calibration", 10);
+
 
         
 
@@ -53,20 +55,18 @@ public:
     subscription_motors_feedback_ = this->create_subscription<interfaces::msg::MotorsFeedback>(
     "motors_feedback", 10, std::bind(&car_control::motorsFeedbackCallback, this, _1));
 
-    subscription_steering_calibration_ = this->create_subscription<interfaces::msg::SteeringCalibration>(
-    "steering_calibration", 10, std::bind(&car_control::steeringCalibrationCallback, this, _1));
-
     subscription_us_emergency_ = this->create_subscription<interfaces::msg::Ultrasonic>(
     "us_data", 10, std::bind(&car_control::EmergencyCallback, this, _1));
 
+    subscription_speed_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+    "speed_command", 10, std::bind(&car_control::SpeedCallback, this, _1));
 
-        server_calibration_ = this->create_service<std_srvs::srv::Empty>(
-                            "steering_calibration", std::bind(&car_control::steeringCalibration, this, std::placeholders::_1, std::placeholders::_2));
+    subscription_steering_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+    "steer_command", 10, std::bind(&car_control::SteerCallback, this, _1));
 
         timer_ = this->create_wall_timer(PERIOD_UPDATE_CMD, std::bind(&car_control::updateCmd, this));
 
         
-        RCLCPP_INFO(this->get_logger(), "car_control_node READY");
     }
 
     
@@ -98,7 +98,6 @@ private:
                 RCLCPP_INFO(this->get_logger(), "Switching to AUTONOMOUS Mode");
             }else if (mode==2){
                 RCLCPP_INFO(this->get_logger(), "Switching to STEERING CALIBRATION Mode");
-                startSteeringCalibration();
             }
         }
         
@@ -139,72 +138,9 @@ private:
         int8_t steeringVal = static_cast<int8_t>(std::round(requestedSteerAngle * 127.0f));
         motorsOrder.steering_angle = steeringVal;
 
-        if (!start){    //Car stopped or Emergency Stop
-
-            leftRearPwmCmd = STOP;
-            rightRearPwmCmd = STOP;
-            //steeringPwmCmd = STOP;
-
-
-        }else{ //Car started
-
-            //Manual Mode
-            if (mode==0){
-                
-                manualPropulsionCmd(requestedThrottle, reverse, leftRearPwmCmd,rightRearPwmCmd);
-
-                //steeringCmd(requestedSteerAngle,currentAngle, steeringPwmCmd);
-
-
-            //Autonomous Mode
-            } else if (mode==1){
-                //...
-            }
-        }
-
-        // Forward + straight limits
-        //if (!reverse && EmergencyStop[1]) stop = true;
-//
-        //// Reverse + straight limits
-        //else if (reverse && EmergencyStop[4]) stop = true;
-
-        if (!reverse){
-            for (int i=0; i<3; i++){
-                if (EmergencyStop[i]){
-                    stop = true;
-                    break;
-                }
-            }
-        }
-
-        else{
-            for (int i=3; i<6; i++){
-                if (EmergencyStop[i]){
-                    stop = true;
-                    break;
-                }
-            }
-        }
-
-        //// Forward + steering right
-        //else if (!reverse && EmergencyStop[0] && motorsOrder.steering_angle > STOP)
-        //    stop = true;
-//
-        //// Forward + steering left
-        //else if (!reverse && EmergencyStop[2] && motorsOrder.steering_angle < STOP)
-        //    stop = true;
-//
-        //// Reverse + steering left
-        //else if (reverse && EmergencyStop[5] && motorsOrder.steering_angle < STOP)
-        //    stop = true;
-//
-        //// Reverse + steering right (rear right)
-        //else if (reverse && EmergencyStop[3] && motorsOrder.steering_angle > STOP)
-        //    stop = true;
 
         if (stop) {
-            leftRearPwmCmd  = STOP;
-            rightRearPwmCmd = STOP;
+            RCLCPP_INFO(this->get_logger(), "we go to 0 in stop");
         }else{
 
             //Send order to motors
@@ -214,68 +150,14 @@ private:
             // steering already computed above
             currentAngle = requestedSteerAngle;
         }
-
         publisher_can_->publish(motorsOrder);
     }
 
-
-    /* Start the steering calibration process :
-    *
-    * Publish a calibration request on the "/steering_calibration" topic
-    */
-    void startSteeringCalibration(){
-
-        auto calibrationMsg = interfaces::msg::SteeringCalibration();
-        calibrationMsg.request = true;
-
-        RCLCPP_INFO(this->get_logger(), "Sending calibration request .....");
-        publisher_steeringCalibration_->publish(calibrationMsg);
-    }
-
-
-    /* Function called by "steering_calibration" service
-    * 1. Switch to calibration mode
-    * 2. Call startSteeringCalibration function
-    */
-    void steeringCalibration([[maybe_unused]] std_srvs::srv::Empty::Request::SharedPtr req,
-                            [[maybe_unused]] std_srvs::srv::Empty::Response::SharedPtr res)
-    {
-
-        mode = 2;    //Switch to calibration mode
-        RCLCPP_WARN(this->get_logger(), "Switching to STEERING CALIBRATION Mode");
-        startSteeringCalibration();
-    }
     
 
-    /* Manage steering calibration process [callback function]  :
-    *
-    * This function is called when a message is published on the "/steering_calibration" topic
-    */
-    void steeringCalibrationCallback (const interfaces::msg::SteeringCalibration::SharedPtr calibrationMsg){
-
-        if (calibrationMsg->in_progress == true && calibrationMsg->user_need == false){
-        RCLCPP_INFO(this->get_logger(), "Steering Calibration in progress, please wait ....");
-
-        } else if (calibrationMsg->in_progress == true && calibrationMsg->user_need == true){
-            RCLCPP_WARN(this->get_logger(), "Please use the buttons (L/R) to center the steering wheels.\nThen, press the blue button on the NucleoF103 to continue");
-        
-        } else if (calibrationMsg->status == 1){
-            RCLCPP_INFO(this->get_logger(), "Steering calibration [SUCCESS]");
-            RCLCPP_INFO(this->get_logger(), "Switching to MANUAL Mode");
-            mode = 0;    //Switch to manual mode
-            start = false;  //Stop car
-        
-        } else if (calibrationMsg->status == -1){
-            RCLCPP_ERROR(this->get_logger(), "Steering calibration [FAILED]");
-            RCLCPP_INFO(this->get_logger(), "Switching to MANUAL Mode");
-            mode = 0;    //Switch to manual mode
-            start = false;  //Stop car
-        }
-    
-    }
 
     /*EMERGENCY STOP*/
-        void EmergencyCallback(const interfaces::msg::Ultrasonic::SharedPtr USMsg) {// in centemetre
+    void EmergencyCallback(const interfaces::msg::Ultrasonic::SharedPtr USMsg) {// in centemetre
             /*I think we have to do some math to get the distance of the obstecle but i'm simplifing it now and saying it gives us directly a distance*/
             EmergencyStop[0] = (USMsg->front_left  < 50);
             EmergencyStop[1] = (USMsg->front_center < 50);
@@ -283,6 +165,27 @@ private:
             EmergencyStop[3] = (USMsg->rear_right  < 50);
             EmergencyStop[4] = (USMsg->rear_center < 50);
             EmergencyStop[5] = (USMsg->rear_left   < 50);
+    }
+
+    void SpeedCallback(const std_msgs::msg::Float64MultiArray Msg) {
+            if((Msg.data[0]/maxSpeed) > 1 || (Msg.data[0]/maxSpeed) < -1){
+                RCLCPP_INFO(this->get_logger(), "I'm bigger then 1");
+                leftRearPwmCmd = 100;
+            }else{
+                leftRearPwmCmd = 50 + Msg.data[0]/maxSpeed *50;
+            }
+            if((Msg.data[1]/maxSpeed) > 1 || (Msg.data[0]/maxSpeed) < -1){
+                rightRearPwmCmd = 100;
+            }else{
+                rightRearPwmCmd = 50 + Msg.data[1]/maxSpeed *50;
+            }
+
+
+    }
+
+    
+    void SteerCallback(const std_msgs::msg::Float64MultiArray Msg) {
+            requestedSteerAngle = Msg.data[0];
     }
 
 
@@ -297,6 +200,7 @@ private:
     // 3: rear_right, 4: rear_center, 5: rear_left
     std::array<bool, 6> EmergencyStop{}; // all false initially
     int mode;    //0 : Manual    1 : Auto    2 : Calibration
+    double maxSpeed = 0.53; // vitesse angulaire du robot
 
     
     //Motors feedback variables
@@ -314,19 +218,16 @@ private:
 
     //Publishers
     rclcpp::Publisher<interfaces::msg::MotorsOrder>::SharedPtr publisher_can_;
-    rclcpp::Publisher<interfaces::msg::SteeringCalibration>::SharedPtr publisher_steeringCalibration_;
 
     //Subscribers
     rclcpp::Subscription<interfaces::msg::JoystickOrder>::SharedPtr subscription_joystick_order_;
     rclcpp::Subscription<interfaces::msg::MotorsFeedback>::SharedPtr subscription_motors_feedback_;
-    rclcpp::Subscription<interfaces::msg::SteeringCalibration>::SharedPtr subscription_steering_calibration_;
     rclcpp::Subscription<interfaces::msg::Ultrasonic>::SharedPtr subscription_us_emergency_;
+    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr subscription_speed_;
+    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr subscription_steering_;
 
     //Timer
     rclcpp::TimerBase::SharedPtr timer_;
-
-    //Steering calibration Service
-    rclcpp::Service<std_srvs::srv::Empty>::SharedPtr server_calibration_;
 };
 
 
